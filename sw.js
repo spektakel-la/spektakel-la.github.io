268d3bc39978e0c9a4735ad1074bcdcdc4582609
elvomka <lkdeatour-githubspektakel@yahoo.com>
2024-05-31 20:09:47 +0200

    trigger app-reloads on service-worker updates

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
+const SW_VERSION = '1.0.0';
+
 importScripts('./assets/3rd-party/workbox-v7.1.0/workbox-sw.js');
 workbox.setConfig({
     // debug: true,
     modulePathPrefix: './assets/3rd-party/workbox-v7.1.0/',
 });
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+self.addEventListener('message', (event) => {
+  if (event.data.type === 'GET_VERSION') {
+    event.ports[0].postMessage(SW_VERSION);
+  }
+
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+const {precacheAndRoute} = workbox.precaching;
+precacheAndRoute(self.__WB_MANIFEST || []);
+
 
 // Use a stale-while-revalidate strategy to handle requests by default.
 // workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate());
