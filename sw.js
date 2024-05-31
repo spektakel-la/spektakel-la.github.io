1d9a8448dd0a6ee7d02d59050fcc9d5a36ecd33f
Lothar Kaiser (FG-754) <Lothar.Kaiser@bmw.de>
2024-05-31 14:58:36 +0200

    resilient precaching conf

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 importScripts('./assets/3rd-party/workbox-v7.1.0/workbox-sw.js');
-
 workbox.setConfig({
     debug: false,
     modulePathPrefix: './assets/3rd-party/workbox-v7.1.0/',
 });
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+
+// Use a stale-while-revalidate strategy to handle requests by default.
+// workbox.routing.setDefaultHandler(new workbox.strategies.StaleWhileRevalidate());
