dc90d3c38e5749c8c214d139056b7510224bde8f
elvomka <lkdeatour-githubspektakel@yahoo.com>
2024-06-11 14:29:04 +0200

    namespacing js functions

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,13 +1,34 @@
 ---
 ---
 
-const LOCATIONS = {{ site.locations | jsonify }};
-const ARTISTS = {{ site.artists | jsonify }};
-const SCHEDULE = {{ site.data.schedule | sort: 'time' | jsonify }};
-const MAP_BOUNDS = {{ site.data.settings.offline_map.bounding_rect | jsonify }};
-const MARKER_ICON_SIZE = {{ site.data.settings.offline_map.icon_size | jsonify }};
-const MARKER_ICON_ANCHOR = {{ site.data.settings.offline_map.icon_anchor | jsonify }};
+(function () {
+    const LOCATIONS = {{ site.locations | jsonify }};
+    const ARTISTS = {{ site.artists | jsonify }};
+    const SCHEDULE = {{ site.data.schedule | sort: 'time' | jsonify }};
+    const MAP_BOUNDS = {{ site.data.settings.offline_map.bounding_rect | jsonify }};
+    const MARKER_ICON_SIZE = {{ site.data.settings.offline_map.icon_size | jsonify }};
+    const MARKER_ICON_ANCHOR = {{ site.data.settings.offline_map.icon_anchor | jsonify }};
 
-const DATA_ATTRIBUTE_FAVORITE_TOGGLE = 'data-artist-favorite-toggle';
-const CSS_CLASS_NO_FAVORITE = 'fa-star-o';
-const CSS_CLASS_FAVORITE = 'fa-star';
+    const DATA_ATTRIBUTE_FAVORITE_TOGGLE = 'data-artist-favorite-toggle';
+    const CSS_CLASS_NO_FAVORITE = 'fa-star-o';
+    const CSS_CLASS_FAVORITE = 'fa-star';
+
+    /*
+     * Namespace setup
+     */
+    const spektakel = window.spektakel || {};
+    spektakel.constants = (function() {
+        return {
+             LOCATIONS,
+             ARTISTS,
+             SCHEDULE,
+             MAP_BOUNDS,
+             MARKER_ICON_SIZE,
+             MARKER_ICON_ANCHOR,
+             DATA_ATTRIBUTE_FAVORITE_TOGGLE,
+             CSS_CLASS_NO_FAVORITE,
+             CSS_CLASS_FAVORITE,
+        }
+    })();
+    window.spektakel = spektakel;
+})();
