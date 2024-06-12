94ea8964f19f05540b0f6f965ff707bcfc3784f8
Lothar Kaiser (FG-754) <Lothar.Kaiser@bmw.de>
2024-06-12 09:49:32 +0200

    remove constants for data-attributes and css-classes

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -9,10 +9,6 @@
     const MARKER_ICON_SIZE = {{ site.data.settings.offline_map.icon_size | jsonify }};
     const MARKER_ICON_ANCHOR = {{ site.data.settings.offline_map.icon_anchor | jsonify }};
 
-    const DATA_ATTRIBUTE_FAVORITE_TOGGLE = 'data-artist-favorite-toggle';
-    const CSS_CLASS_NO_FAVORITE = 'fa-star-o';
-    const CSS_CLASS_FAVORITE = 'fa-star';
-
     /*
      * Namespace setup
      */
@@ -25,9 +21,6 @@
              MAP_BOUNDS,
              MARKER_ICON_SIZE,
              MARKER_ICON_ANCHOR,
-             DATA_ATTRIBUTE_FAVORITE_TOGGLE,
-             CSS_CLASS_NO_FAVORITE,
-             CSS_CLASS_FAVORITE,
         }
     })();
     window.spektakel = spektakel;
