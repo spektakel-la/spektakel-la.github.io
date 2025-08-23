6a253a3cb2a6f9cd574b0940df1d56d9ff0e56a7
elvomka <lkdeatour-githubspektakel@yahoo.com>
2025-08-23 19:59:04 +0200

    fix: translations for favorite-toggles; successive acts now show up

--- assets/js/page-locations.js
+++ assets/js/page-locations.js
@@ -1,117 +1,163 @@
 (function () {
-
-    const createIconMarkup = (locationObj) => {
-        let label = '';
-        if (locationObj.location_label === 'i') {
-            label =  `<i class="fa fa-info" aria-hidden="true"></i>`;
-        } else {
-            label = locationObj?.location_label ? locationObj.location_label : locationObj.location_id
-        }
-        return `
+  const createIconMarkup = (locationObj) => {
+    let label = "";
+    if (locationObj.location_label === "i") {
+      label = `<i class="fa fa-info" aria-hidden="true"></i>`;
+    } else {
+      label = locationObj?.location_label
+        ? locationObj.location_label
+        : locationObj.location_id;
+    }
+    return `
             <div class="spektakel-leaflet-location-icon-container">
                 <div>
                     ${label}
                 </div>
-            </div>`
-    };
+            </div>`;
+  };
 
-    const toggleArtistDetails = (rowElement) => {
-        rowElement.classList.toggle('expanded');
-        const element = rowElement.nextElementSibling;
-        element.classList.toggle('visible');
-        element.scrollIntoView({behavior: "smooth", block: "center"});
-    };
+  const toggleArtistDetails = (rowElement) => {
+    rowElement.classList.toggle("expanded");
+    const element = rowElement.nextElementSibling;
+    element.classList.toggle("visible");
+    element.scrollIntoView({ behavior: "smooth", block: "center" });
+  };
 
-    const sanityCheckSchedule = () => {
-        spektakel.constants.SCHEDULE.forEach((entry) => {
-            const locationForEntry = spektakel.constants.LOCATIONS.find((loc) => entry.location_id === loc.location_id);
-            if (!locationForEntry) {
-                throw new Error(`Schedule entry with invalid location_id: '${JSON.stringify(entry)}'`);
-            }
-            const artistForEntry = spektakel.constants.ARTISTS.find((art) => entry.artist_id === art.artist_id);
-            if (!artistForEntry) {
-                throw new Error(`Schedule entry with invalid artist_id: '${JSON.stringify(entry)}'`);
-            }
-        });
+  const sanityCheckSchedule = () => {
+    spektakel.constants.SCHEDULE.forEach((entry) => {
+      const locationForEntry = spektakel.constants.LOCATIONS.find(
+        (loc) => entry.location_id === loc.location_id
+      );
+      if (!locationForEntry) {
+        throw new Error(
+          `Schedule entry with invalid location_id: '${JSON.stringify(entry)}'`
+        );
+      }
+      const artistForEntry = spektakel.constants.ARTISTS.find(
+        (art) => entry.artist_id === art.artist_id
+      );
+      if (!artistForEntry) {
+        throw new Error(
+          `Schedule entry with invalid artist_id: '${JSON.stringify(entry)}'`
+        );
+      }
+    });
 
-        spektakel.constants.NIGHTLIFE.forEach((entry) => {
-            const locationForEntry = spektakel.constants.LOCATIONS.find((loc) => entry.location_id === loc.location_id);
-            if (!locationForEntry) {
-                throw new Error(`Schedule entry with invalid location_id: '${JSON.stringify(entry)}'`);
-            }
-            const artistForEntry = spektakel.constants.ARTISTS.find((art) => entry.artist_id === art.artist_id);
-            if (!artistForEntry) {
-                throw new Error(`Schedule entry with invalid artist_id: '${JSON.stringify(entry)}'`);
-            }
-        });
-    };
+    spektakel.constants.NIGHTLIFE.forEach((entry) => {
+      const locationForEntry = spektakel.constants.LOCATIONS.find(
+        (loc) => entry.location_id === loc.location_id
+      );
+      if (!locationForEntry) {
+        throw new Error(
+          `Schedule entry with invalid location_id: '${JSON.stringify(entry)}'`
+        );
+      }
+      const artistForEntry = spektakel.constants.ARTISTS.find(
+        (art) => entry.artist_id === art.artist_id
+      );
+      if (!artistForEntry) {
+        throw new Error(
+          `Schedule entry with invalid artist_id: '${JSON.stringify(entry)}'`
+        );
+      }
+    });
+  };
 
-    const createPopupMarkupForLocation = (locationObj, lang) => {
-        const locationId = locationObj.location_id;
-        const artistFavorites = spektakel.favorites.getArtistFavorites();
+  const createPopupMarkupForLocation = (locationObj, lang) => {
+    const locationId = locationObj.location_id;
+    const artistFavorites = spektakel.favorites.getArtistFavorites();
 
-        const schedule = locationObj.nightlife ? spektakel.constants.NIGHTLIFE : spektakel.constants.SCHEDULE;
-        const scheduleForLocation = schedule.filter((entry) => entry.location_id === locationId);
+    const schedule = locationObj.nightlife
+      ? spektakel.constants.NIGHTLIFE
+      : spektakel.constants.SCHEDULE;
+    const scheduleForLocation = schedule.filter(
+      (entry) => entry.location_id === locationId
+    );
 
-        /*
-         * The schedule is setup with 30 minutes blocks. If an artist acts for 1 hour, he occupies 2 blocks.
-         * The user doesn't care about these blocks, so we just remove successive entries with the same artist.
-         */
-        const prunedScheduleForLocation= scheduleForLocation.reduce((acc, entry) => {
-            const maybeLastEntry = acc[acc.length - 1];
-            if (maybeLastEntry &&
-                maybeLastEntry.artist_id === entry.artist_id &&
-                dateFns.differenceInHours(dateFns.parseISO(maybeLastEntry.time), dateFns.parseISO(entry.time)) < 1
-            ) {
-                return acc;
-            } else {
-                return [...acc, entry];
-            }
-        }, []);
+    /*
+     * The schedule is setup with 30 minutes blocks. If an artist acts for 1 hour, he occupies 2 blocks.
+     * The user doesn't care about these blocks, so we just remove successive entries with the same artist.
+     */
+    const prunedScheduleForLocation = scheduleForLocation.reduce(
+      (acc, entry) => {
+        const maybeLastEntry = acc[acc.length - 1];
+        if (
+          maybeLastEntry &&
+          maybeLastEntry.artist_id === entry.artist_id &&
+          dateFns.differenceInHours(
+            dateFns.parseISO(entry.time),
+            dateFns.parseISO(maybeLastEntry.time)
+          ) < 1
+        ) {
+          return acc;
+        } else {
+          return [...acc, entry];
+        }
+      },
+      []
+    );
 
-        const scheduleForLocationWithArtist = prunedScheduleForLocation.map((entry) => {
-            const artist = spektakel.constants.ARTISTS.find((artist) => artist.artist_id === entry.artist_id);
-            if (artist){
-                entry.artist_name = artist?.short_name ? artist?.short_name : artist?.name;
-                entry.artist_categories = artist?.categories;
-                entry.artist_image = artist?.image;
-                entry.image_position = artist?.image_position;
-                return entry;
-            } else {
-                console.error("Error processing schedule entry");
-                console.dir(entry);
-            }
-        });
+    const scheduleForLocationWithArtist = prunedScheduleForLocation.map(
+      (entry) => {
+        const artist = spektakel.constants.ARTISTS.find(
+          (artist) => artist.artist_id === entry.artist_id
+        );
+        if (artist) {
+          entry.artist_name = artist?.short_name
+            ? artist?.short_name
+            : artist?.name;
+          entry.artist_categories = artist?.categories;
+          entry.artist_image = artist?.image;
+          entry.image_position = artist?.image_position;
+          return entry;
+        } else {
+          console.error("Error processing schedule entry");
+          console.dir(entry);
+        }
+      }
+    );
 
-        let sectionDateString = null;
-        const tableRows = scheduleForLocationWithArtist.map((sched, idx) => {
-            const now = new Date();
-            const scheduleDate = dateFns.parseISO(sched.time);
-            if (now > scheduleDate) {  // ignore old dates
-                return '';
-            }
+    let sectionDateString = null;
+    const tableRows = scheduleForLocationWithArtist
+      .map((sched, idx) => {
+        const now = new Date();
+        const scheduleDate = dateFns.parseISO(sched.time);
+        if (now > scheduleDate) {
+          // ignore old dates
+          return "";
+        }
 
-            const currentDateString = scheduleDate.toLocaleDateString();
-            let maybeDateSection = '';
-            if (currentDateString !== now.toLocaleDateString() // don't show date-section for today
-                && currentDateString !== sectionDateString) {
-                maybeDateSection = `
+        const currentDateString = scheduleDate.toLocaleDateString();
+        let maybeDateSection = "";
+        if (
+          currentDateString !== now.toLocaleDateString() && // don't show date-section for today
+          currentDateString !== sectionDateString
+        ) {
+          maybeDateSection = `
                     <tr class="day-section">
                         <td colspan="2">
-                            <strong>${dateFns.format(scheduleDate, 'EEEE dd.MM.yyyy', { locale: dateFns.locale.de })}</strong>
+                            <strong>${dateFns.format(
+                              scheduleDate,
+                              "EEEE dd.MM.yyyy",
+                              { locale: dateFns.locale.de }
+                            )}</strong>
                         </td>
                     </tr>`;
-                sectionDateString = currentDateString;
-            }
+          sectionDateString = currentDateString;
+        }
 
-            return `
+        return `
                 ${maybeDateSection}
-                <tr class="schedule-details${artistFavorites.includes(sched.artist_id) ? ' favorite': ''}"
-                    data-artist-id="${sched.artist_id}" onclick="spektakel.locations.toggleArtistDetails(this);">
+                <tr class="schedule-details${
+                  artistFavorites.includes(sched.artist_id) ? " favorite" : ""
+                }"
+                    data-artist-id="${
+                      sched.artist_id
+                    }" onclick="spektakel.locations.toggleArtistDetails(this);">
                     <td>
                         <i class="expand-icon fa fa-caret-down" aria-hidden="true"></i>
                         <i class="collapse-icon fa fa-caret-up" aria-hidden="true"></i>
-                        &nbsp;${dateFns.format(scheduleDate, 'HH:mm')}
+                        &nbsp;${dateFns.format(scheduleDate, "HH:mm")}
                     </td>
                     <td>
                         <div class="artist-name">
@@ -121,16 +167,20 @@
                 </tr>
                 <tr class="artist-details">
                     <td colspan="2">
-                        ${spektakel.popover.createPopoverContentHtml(sched.artist_id, lang)}
+                        ${spektakel.popover.createPopoverContentHtml(
+                          sched.artist_id,
+                          lang
+                        )}
                     </td>
                 </tr>
             `;
-        }).join('\n');
+      })
+      .join("\n");
 
-        if(!tableRows){
-            return '';
-        } else {
-            return `
+    if (!tableRows) {
+      return "";
+    } else {
+      return `
                 <table>
                     <tr class="sticky">
                         <th class="schedule-time">Zeit</th>
@@ -139,12 +189,14 @@
                     ${tableRows}
                 </table>
                 `;
-        }
     }
+  };
 
-    const createPopupMarkup = (locationObj, lang) => {
-        const linkToProgram = locationObj.nightlife ? '/nightlife' : `/program#${locationObj.location_id}`;
-        return `
+  const createPopupMarkup = (locationObj, lang) => {
+    const linkToProgram = locationObj.nightlife
+      ? "/nightlife"
+      : `/program#${locationObj.location_id}`;
+    return `
         <div class="leaflet-popup-content">
             <div class="location-title-grid">
                 <div class="location-title">
@@ -165,88 +217,101 @@
             </div>
         </div>
         `;
+  };
+
+  const setupLeafletMap = async (mapContainer, lang) => {
+    const map = L.map(mapContainer, {
+      minZoom: 16,
+      maxZoom: 19,
+      maxBounds: spektakel.constants.MAP_BOUNDS,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    });
+    map.attributionControl.setPrefix(false);
+
+    const hasWebpSupport = await spektakel.featuredetection.hasWebpSupport();
+    const tilesLayer = L.tileLayer(
+      `/assets/img/map/tiles/{z}/{x}/{y}.${hasWebpSupport ? "webp" : "jpg"}`,
+      { minZoom: 16, maxZoom: 19, tms: false, attribution: "© OpenStreetMap" }
+    );
+    tilesLayer.addTo(map);
+
+    const geoLocation = L.control.locate({
+      strings: {
+        title: "Zeig mir wo ich bin",
+        metersUnit: "Meter",
+        feetUnit: "Fuß",
+        popup: "Du befindest dich innerhalb {distance} {unit} von diesem Punkt",
+        outsideMapBoundsMsg:
+          "Du scheinst Dich außerhalb der verfügbaren Kartenregion zu befinden",
+      },
+    });
+    geoLocation.addTo(map);
+
+    const fullScreen = L.control.fullscreen({
+      position: "topright",
+      forceSeparateButton: true,
+    });
+    fullScreen.addTo(map);
+
+    const markers = spektakel.constants.LOCATIONS.map((location) => {
+      const markerColor = location.marker_color
+        ? location.marker_color
+        : "pink";
+      const markerIcon = L.divIcon({
+        className: `spektakel-leaflet-location-icon-${markerColor}`,
+        html: createIconMarkup(location),
+        iconSize: spektakel.constants.MARKER_ICON_SIZE,
+        iconAnchor: spektakel.constants.MARKER_ICON_ANCHOR,
+      });
+
+      const [lat, lon] = location.gps;
+      const marker = L.marker([lat, lon], { icon: markerIcon });
+      marker
+        .bindPopup("", {
+          className: "spektakel-leaflet-popup",
+          offset: [0, -38],
+        })
+        .on("popupopen", (event) => {
+          history.replaceState(null, null, `#${location.location_id}`);
+          event.popup.setContent(createPopupMarkup(location, lang));
+        })
+        .on("popupclose", (event) => {
+          history.replaceState(null, null, " ");
+        });
+      marker.addTo(map);
+
+      // debug position
+      // L.marker([lat, lon]).addTo(map);
+
+      return { marker, location_id: location.location_id };
+    });
+
+    map.on("load", () => {
+      const hash = window.location.hash.substring(1); // Entfernt das '#' Zeichen
+      const targetMarker = markers.find((m) => m.location_id === hash);
+      if (targetMarker) {
+        targetMarker.marker.openPopup();
+      }
+    });
+
+    map.fitBounds(
+      L.latLngBounds(
+        spektakel.constants.LOCATIONS.map((location) => location.gps)
+      )
+    );
+  };
+
+  /*
+   * Namespace setup
+   */
+  const spektakel = window.spektakel || {};
+  spektakel.locations = (function () {
+    return {
+      toggleArtistDetails,
+      sanityCheckSchedule,
+      setupLeafletMap,
     };
-
-    const setupLeafletMap = async (mapContainer, lang) => {
-        const map = L.map(mapContainer, {
-            minZoom: 16,
-            maxZoom: 19,
-            maxBounds: spektakel.constants.MAP_BOUNDS,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        });
-        map.attributionControl.setPrefix(false)
-
-        const hasWebpSupport = await spektakel.featuredetection.hasWebpSupport();
-        const tilesLayer = L.tileLayer(`/assets/img/map/tiles/{z}/{x}/{y}.${hasWebpSupport ? 'webp' : 'jpg'}`,
-            { minZoom: 16, maxZoom: 19, tms: false, attribution: '© OpenStreetMap'});
-        tilesLayer.addTo(map);
-
-        const geoLocation = L.control.locate({
-            strings: {
-                title: "Zeig mir wo ich bin",
-                metersUnit: "Meter",
-                feetUnit: "Fuß",
-                popup: "Du befindest dich innerhalb {distance} {unit} von diesem Punkt",
-                outsideMapBoundsMsg: "Du scheinst Dich außerhalb der verfügbaren Kartenregion zu befinden"
-            }
-        });
-        geoLocation.addTo(map);
-
-        const fullScreen = L.control
-        .fullscreen({
-            position: 'topright',
-            forceSeparateButton: true,
-        });
-        fullScreen.addTo(map);
-
-        const markers = spektakel.constants.LOCATIONS.map((location) => {
-            const markerColor = location.marker_color ? location.marker_color : 'pink';
-            const markerIcon = L.divIcon({
-            className: `spektakel-leaflet-location-icon-${markerColor}`,
-                html: createIconMarkup(location),
-                iconSize: spektakel.constants.MARKER_ICON_SIZE,
-                iconAnchor: spektakel.constants.MARKER_ICON_ANCHOR
-            });
-
-            const [lat, lon] = location.gps;
-            const marker = L.marker([lat, lon], {icon: markerIcon});
-            marker.bindPopup('', {className: 'spektakel-leaflet-popup', offset: [0, -38]})
-                .on("popupopen", (event) => {
-                    history.replaceState(null, null, `#${location.location_id}`);
-                    event.popup.setContent(createPopupMarkup(location, lang));
-                })
-                .on("popupclose", (event) => {
-                    history.replaceState(null, null, ' ');
-                });
-            marker.addTo(map);
-
-            // debug position
-            // L.marker([lat, lon]).addTo(map);
-
-            return { marker, location_id: location.location_id };
-        });
-
-        map.on('load', () => {
-            const hash = window.location.hash.substring(1); // Entfernt das '#' Zeichen
-            const targetMarker = markers.find(m => m.location_id === hash);
-            if (targetMarker) {
-                targetMarker.marker.openPopup();
-            }
-        });
-
-        map.fitBounds(L.latLngBounds(spektakel.constants.LOCATIONS.map(location => location.gps)));
-    }
-
-    /*
-     * Namespace setup
-     */
-    const spektakel = window.spektakel || {};
-    spektakel.locations = (function() {
-        return {
-            toggleArtistDetails,
-            sanityCheckSchedule,
-            setupLeafletMap
-        }
-    })();
-    window.spektakel = spektakel;
+  })();
+  window.spektakel = spektakel;
 })();
--- assets/js/popover.js
+++ assets/js/popover.js
@@ -1,51 +1,55 @@
 (function () {
-
-    const createPopoverContentImageHtml = (artist) => {
-        const artistImageWebp = artist.image;
-        if (artistImageWebp) {
-            const artistImageJpg = artistImageWebp.replace('.webp', '.jpg');
-            return `
+  const createPopoverContentImageHtml = (artist) => {
+    const artistImageWebp = artist.image;
+    if (artistImageWebp) {
+      const artistImageJpg = artistImageWebp.replace(".webp", ".jpg");
+      return `
                 <picture>
                     <source srcset="${artistImageWebp}" type="image/webp">
                     <source srcset="${artistImageJpg}" type="image/png">
                     <img src="${artistImageJpg}"
                         alt="${artist.name}"
-                        class="custom-position-${artist.image_position ? artist.image_position : 'center'}">
+                        class="custom-position-${
+                          artist.image_position
+                            ? artist.image_position
+                            : "center"
+                        }">
                 </picture>
             `;
-        } else {
-            return '';
-        }
-    };
-
-    const createPopoverContentCategoryHtml = (artist, lang) => {
-        if (!artist[lang]?.categories) {
-            return '';
-        }
-        return `(${artist[lang].categories.join(', ')})`;
+    } else {
+      return "";
     }
+  };
 
-    const createPopoverContentHtml = (artistId, lang) => {
-        const artist = spektakel.constants.ARTISTS.find(a => a.artist_id === artistId);
-        const artistFavorites = spektakel.favorites.getArtistFavorites();
+  const createPopoverContentCategoryHtml = (artist, lang) => {
+    if (!artist[lang]?.categories) {
+      return "";
+    }
+    return `(${artist[lang].categories.join(", ")})`;
+  };
 
-        if(!artist) {
-            return '';
-        } else {
+  const createPopoverContentHtml = (artistId, lang) => {
+    const artist = spektakel.constants.ARTISTS.find(
+      (a) => a.artist_id === artistId
+    );
+    const artistFavorites = spektakel.favorites.getArtistFavorites();
 
-            let artistLinkTitle;
-            let artistLink;
-            if (lang === 'de') {
-                artistLinkTitle = 'Zum Künstlerprofil';
-                artistLink = `/artists#${artistId}`
-            } else if (lang === 'en'){
-                artistLinkTitle = "Visit artist profile";
-                artistLink = `/${lang}/artists#${artistId}`
-            } else {
-                throw new Error(`Language not supported: "${lang}"`)
-            }
+    if (!artist) {
+      return "";
+    } else {
+      let artistLinkTitle;
+      let artistLink;
+      if (lang === "de") {
+        artistLinkTitle = "Zum Künstlerprofil";
+        artistLink = `/artists#${artistId}`;
+      } else if (lang === "en") {
+        artistLinkTitle = "Visit artist profile";
+        artistLink = `/${lang}/artists#${artistId}`;
+      } else {
+        throw new Error(`Language not supported: "${lang}"`);
+      }
 
-            return `
+      return `
                 <div class="popover-artist-details">
                     <div class="popover-artist-details-image">
                         ${createPopoverContentImageHtml(artist)}
@@ -56,26 +60,28 @@
                     <div class="popover-artist-details-link">
                         <a href="${artistLink}">${artistLinkTitle}</a>
                         <span></span>
-                        <i class="favorite-toggle fa ${artistFavorites.includes(artistId) ?
-                                                        'fa-star' :
-                                                        'fa-star-o'}"
+                        <i class="favorite-toggle fa ${
+                          artistFavorites.includes(artistId)
+                            ? "fa-star"
+                            : "fa-star-o"
+                        }"
                         aria-hidden="true"
                         data-artist-id="${artistId}"
-                        onclick="spektakel.favorites.toggleArtistFavorite('${artistId}');"></i>
+                        onclick="spektakel.favorites.toggleArtistFavorite('${artistId}', '${lang}');"></i>
                     </div>
                 </div>
             `;
-        }
-    };
+    }
+  };
 
-    /*
-     * Namespace setup
-     */
-    const spektakel = window.spektakel || {};
-    spektakel.popover = (function() {
-        return {
-            createPopoverContentHtml,
-        }
-    })();
-    window.spektakel = spektakel;
+  /*
+   * Namespace setup
+   */
+  const spektakel = window.spektakel || {};
+  spektakel.popover = (function () {
+    return {
+      createPopoverContentHtml,
+    };
+  })();
+  window.spektakel = spektakel;
 })();
