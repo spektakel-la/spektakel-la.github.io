905c62c57cd1b9fc136004783ab394e61101c033
elvomka <lkdeatour-githubspektakel@yahoo.com>
2025-09-18 23:45:34 +0200

    fix: make sure to cache all fonts in the sw

--- workbox-config.js
+++ workbox-config.js
@@ -1,31 +1,29 @@
 // https://developer.chrome.com/docs/workbox/modules/workbox-build#method-injectManifest
 module.exports = {
-	globDirectory: '_site/',
-	globPatterns: [
-		'**/*.{html,json,css,png,jpg,jpeg,webp,svg,js,mjs,ico,woff,woff2}'
-	],
-	globIgnores: [
-		'workbox-config.js',
+  globDirectory: '_site/',
+  globPatterns: ['**/*.{html,json,css,png,jpg,jpeg,webp,svg,js,mjs,ico,woff,woff2,ttf}'],
+  globIgnores: [
+    'workbox-config.js',
 
-		// we only cache webp and ignore the jpg/png-siblings
-		'assets/img/map/*.jpg',
-		'assets/img/artists/*.jpg',
-		'assets/img/youtube/*.jpg',
-		'assets/img/youtube/*.png',
+    // we only cache webp and ignore the jpg/png-siblings
+    'assets/img/map/*.jpg',
+    'assets/img/artists/*.jpg',
+    'assets/img/youtube/*.jpg',
+    'assets/img/youtube/*.png',
 
-		// some sections/categories won't be precached
-		'assets/img/map/**/*',
-		'assets/img/screenshots/**/*',
-		'assets/img/impressions/**/*',
-		'assets/img/sponsors/**/*',
+    // some sections/categories won't be precached
+    'assets/img/map/**/*',
+    'assets/img/screenshots/**/*',
+    'assets/img/impressions/**/*',
+    'assets/img/sponsors/**/*',
 
-		// Obsolete fonts
-		'assets/3rd-party/font-awesome/**/*.svg',
+    // Obsolete fonts
+    'assets/3rd-party/font-awesome/**/*.svg',
 
-		// Duplications due to i18n
-		'en/assets/js/**/*',
-		'en/assets/css/**/*',
-	],
-	swSrc: 'sw.js',
-	swDest: '_site/sw.js'
+    // Duplications due to i18n
+    'en/assets/js/**/*',
+    'en/assets/css/**/*',
+  ],
+  swSrc: 'sw.js',
+  swDest: '_site/sw.js',
 };
